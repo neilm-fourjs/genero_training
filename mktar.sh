0a5447ca3d3c3de1ff44197703e4a68a9b2e07fb
Neil Martin <neilm@4js.com>
2022-11-24 13:32:01 +0000

    mktar makes zip

--- mktar.sh
+++ mktar.sh
@@ -1,2 +1,5 @@
 make clean
-tar cvzf training.tgz training.4pw db env.sh etc getGDCcfg makefile mk_db arrays orders query reports/*.4gl reports/*.inc 
+touch *.4pw
+rm -f training.tgz training.zip
+tar cvzf training.tgz *.4pw db env.sh etc getGDCcfg makefile mk_db arrays orders query reports/*.4gl reports/*.inc 
+zip -ro training.zip *.4pw db env.sh etc getGDCcfg makefile mk_db arrays orders query reports/*.4gl reports/*.inc 
